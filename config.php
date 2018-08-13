e1d6a17c64eb9106663d779495b2cff5a7dcca50
cooper-ajc <41577537+cooper-ajc@users.noreply.github.com>
2018-08-13 15:29:45 +0100

    new users

--- config.php
+++ config.php
@@ -1,12 +1,12 @@
 <?php 
 	session_start();
 	// connect to database
-	$conn = mysqli_connect("localhost", "root", "root", "complete-blog-php");
+	$conn = mysqli_connect("www.db4free.net", "cooperaj", "09cooperA", "astonevents1997");
 
 	if (!$conn) {
 		die("Error connecting to database: " . mysqli_connect_error());
 	}
     // define global constants
 	define ('ROOT_PATH', realpath(dirname(__FILE__)));
-	define('BASE_URL', 'http://localhost/complete-blog-php/');
+	define('BASE_URL', 'http://localhost/astonevents1997/');
 ?>
