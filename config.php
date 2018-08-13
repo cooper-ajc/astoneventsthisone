3d182c1594a581baa775b66731223d11fcf6c350
cooper-ajc <41577537+cooper-ajc@users.noreply.github.com>
2018-08-13 15:53:28 +0100

    lc

--- config.php
+++ config.php
@@ -1,20 +1,12 @@
 <?php 
+	session_start();
+	// connect to database
+	$conn = mysqli_connect("localhost:3306", "cooperaj", "09cooperA", "astonevents1997");
 
-session_start();
-$username = cooperaj; 
-$password = 09cooperA; 
-$host = "db4free.net:3306"; 
-$dbname = astonevents1997; 
-
-$options = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'); 
-
-try 
-{ 
-    $db = new PDO("mysql:host={$host};dbname={$dbname};charset=utf8", $username, $password, $options); 
-} 
-catch(PDOException $ex) 
-{ 
-    die("Failed to connect to the database MUHAHAHAHHAH: " . $ex->getMessage()); 
-} 
-
+	if (!$conn) {
+		die("Error connecting to database I hate myself much: " . mysqli_connect_error());
+	}
+    // define global constants
+	define ('ROOT_PATH', realpath(dirname(__FILE__)));
+	define('BASE_URL', 'www.db4free.net');
 ?>
