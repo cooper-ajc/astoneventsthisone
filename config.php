e952879b45279ba9c8763ee737b75dc70ad9fd0f
cooper-ajc <41577537+cooper-ajc@users.noreply.github.com>
2018-08-13 15:41:31 +0100

    error

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php 
 	session_start();
 	// connect to database
-	$conn = mysqli_connect("www.db4free.net", "cooperaj", "09cooperA", "astonevents1997");
+	$conn = mysqli_connect("db4free.net", "cooperaj", "09cooperA", "astonevents1997");
 
 	if (!$conn) {
-		die("Error connecting to database: " . mysqli_connect_error());
+		die("Error connecting to database I hate myself much: " . mysqli_connect_error());
 	}
     // define global constants
 	define ('ROOT_PATH', realpath(dirname(__FILE__)));
