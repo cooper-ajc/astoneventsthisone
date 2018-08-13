d2accedc7f6337b8aa092a538cc8e6b2f23709af
cooper-ajc <41577537+cooper-ajc@users.noreply.github.com>
2018-08-13 15:43:38 +0100

    new one

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php 
 	session_start();
 	// connect to database
-	$conn = mysqli_connect("db4free.net", "cooperaj", "09cooperA", "astonevents1997");
+	$conn = mysqli_connect("db4free.net:3306", "cooperaj", "09cooperA", "astonevents1997");
 
 	if (!$conn) {
 		die("Error connecting to database I hate myself much: " . mysqli_connect_error());
@@ -10,4 +10,3 @@
 	define ('ROOT_PATH', realpath(dirname(__FILE__)));
 	define('BASE_URL', 'www.db4free.net');
 ?>
-
